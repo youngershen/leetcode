028be996e23747c2205b11ebadbb523025136ade
Younger Shen <shenyangang@163.com>
2021-08-18 22:38:02 +0800

    SAVE: 增加快速排序相关代码

--- dsa/sort/quick_sort.c
+++ dsa/sort/quick_sort.c
@@ -1,18 +1,11 @@
 /*
  * Project: leetcode
- * File : sort.h
- * Date : 2021/8/9 15:45
+ * File : quick_sort.c
+ * Date : 2021/8/18 22:37
  * Author: Younger Shen <申延刚>
  * Web: https://github.com/youngershen
  * Cell: 13811754531
  * Email : shenyangang@163.com
 */
 
-#ifndef LEETCODE_SORT_H
-#define LEETCODE_SORT_H
-
-#include "bubble_sort.h"
-#include "insert_sort.h"
 #include "quick_sort.h"
-
-#endif //LEETCODE_SORT_H
---
+++ dsa/sort/quick_sort.h
@@ -0,0 +1,14 @@
+/*
+ * Project: leetcode
+ * File : quick_sort.h
+ * Date : 2021/8/18 22:37
+ * Author: Younger Shen <申延刚>
+ * Web: https://github.com/youngershen
+ * Cell: 13811754531
+ * Email : shenyangang@163.com
+*/
+
+#ifndef LEETCODE_QUICK_SORT_H
+#define LEETCODE_QUICK_SORT_H
+
+#endif //LEETCODE_QUICK_SORT_H
