74069fd79e34600a1c6c2a287e01d4af682e6920
Younger Shen <shenyangang@163.com>
2021-10-10 22:30:43 +0800

    Save: 保存代码

--- types/seq_list.h
+++ types/seq_list.h
@@ -41,7 +41,6 @@ DATA seq_list_get(SEQ_LIST_HEAD head, INT index);
 INT seq_list_rest_buffer(SEQ_LIST_HEAD head);
 
 INT seq_list_is_full(SEQ_LIST_HEAD head);
-INT seq_list_is_full(SEQ_LIST_HEAD head);
 
 
 
